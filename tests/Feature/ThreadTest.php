c5c8cd397ef9f7769baf5e1024a2422813f36883
Rwarlock <bhanukrsingh@gmail.com>
2020-02-23 21:29:36 +0530

    initial commit

---
+++ tests/Feature/ThreadTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ThreadTest extends TestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A basic test example.
+     */
+    public function a_user_can_browse_threads()
+    {
+        $response = $this->get('/threads');
+
+        $response->assertStatus(200);
+    }
+}
